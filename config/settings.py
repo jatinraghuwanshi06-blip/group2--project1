9955f0e678322bd8ed26d5ecfad6814dd54c75c3
vaishnavi141003 <vaishnavigollapalli2003@gmail.com>
2026-06-23 00:24:20 +0530

    test: validate PDF extraction piprline

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'legaltech_db',
         'USER': 'postgres',
-        'PASSWORD': 'jatin9131',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
